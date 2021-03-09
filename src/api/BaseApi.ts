e61f1987375f6aac7e9b18edcc058a5c141c4589
Vladeksh <vladeksh@gmail.com>
2021-03-09 03:46:51 -0300

    Agrega configuración faltante para levantar aplicación en Heroku.

--- src/api/BaseApi.ts
+++ src/api/BaseApi.ts
@@ -1,7 +1,7 @@
 import { ApiInterface } from "@/types/types";
 
 export class BaseApi implements ApiInterface {
-  protected baseUrl = "http://localhost:3000/";
+  protected baseUrl = process.env.VUE_APP_API_BASE_URL;
   // eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/no-unused-vars,@typescript-eslint/no-explicit-any
   async fetch(url: string): Promise<any> {}
 }
